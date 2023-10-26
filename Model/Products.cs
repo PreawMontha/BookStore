2e6f3459b8b365379de60dc397ce6dbe5d16daa1
PreawMontha <mon2544.p@gmail.com>
2023-10-26 14:02:46 +0700

    Initial commit

---
+++ Model/Products.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BookStore.Model
+{
+    public class Products
+    {
+        public string? ProductName { get; set; }
+        public int Price { get; set; }
+
+
+        public List<Products> GetProductData()
+        {
+            MySqlConnection conn;
+            string server = "localhost";
+            string database = "bookstore";
+            string uid = "root";
+            string password = "12345678";
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";SSL Mode=None;";
+            conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            string sqlCmd = "SELECT ProductName, Price FROM products LIMIT 3 ";
+            MySqlCommand cmd = new MySqlCommand(sqlCmd, conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            /* Products product = new Products();*/
+            List<Products> productsList = new List<Products>();
+            while (dr.Read())
+            {
+                Products productsItem = new Products();
+                productsItem.ProductName = dr["ProductName"].ToString();
+                productsItem.Price = Convert.ToInt32(dr["Price"].ToString());
+
+
+                productsList.Add(productsItem);
+            }
+            conn.Close();
+            return productsList;
+        }
+    }
+}
+
+
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using BookStore.Model;
+
+
+namespace BookStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+
+    }
+}
